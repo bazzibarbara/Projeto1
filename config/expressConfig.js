d318640a23ca4b4f8294a8fd5ad4c9ea13876696
unknown <barbararamosberno@outlook.com>
2023-03-31 09:47:38 -0300

    atualização

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -7,6 +7,6 @@ app.use(express.urlencoded({
     extended:true
 }));
 
-const musicaRouter=  require('../src/domains/musicas/controllers/index');
-app.use('api/musica', musicaRouter);
+const musicaRouter=  require('../src/controllers/index');
+app.use('/api/musica', musicaRouter);
 module.exports = app;
