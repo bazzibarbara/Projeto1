b302b920588d828baabf41f4ee8816498bfe42ae
iagozag <iagozag@gmail.com>
2023-03-31 20:22:32 -0300

    metodo get para encontrar musica por nome adicionado

--- src/domains/musicas/controllers/index.js
+++ src/domains/musicas/controllers/index.js
@@ -2,13 +2,27 @@ const express= require ('express');
 const router = express.Router();
 const Musica = require('../models/Musica');
 
-router.get('/', (req,res) => {
+router.get('/all', (req,res) => {
     res.status(200).send(Musica);
 });
 
-router.get('/name', (req,res) =>{ 
-    const { nome } = req.query; //obrigatoriamente precisa passar o parametro
-    res.status(200).send(nome);
+// retorna dados de uma musica pelo name
+router.get('/all/:name', (req,res) =>{  //obrigatoriamente precisa passar o parametro
+    const { name } = req.params;
+    const musica = Musica.find(musica => musica.nome === name);
+
+    if(!musica) return res.status(404).json();
+
+    res.json(musica);
+});
+
+router.post('/all', (req, res) => {
+    const { name, artista, genero, quantidadeDownloads } = req.body;
+    
+    const  musica = Musica.find(musica => musica.name == name);
+    if(musica) return res.status(401).json();
+
+    res.json({name, artista, genero, quantidadeDownloads});
 });
 
 module.exports = router;
