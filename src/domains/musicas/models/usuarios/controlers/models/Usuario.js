b0b6606286c6800ba010a7dd009ab813b9430ff2
unknown <barbararamosberno@outlook.com>
2023-04-05 18:28:59 -0300

    tentando organizar as pastas e o usuario.js

---
+++ src/domains/musicas/models/usuarios/controlers/models/Usuario.js
@@ -0,0 +1,33 @@
+const sequelize = require('../../../database/index');
+const {Datatypes} = require('sequelize');
+
+const Usuario = sequelize.define('Usuario',{
+    id : {
+        type: Datatypes.INTEGER,
+        primaruKey: true,
+        autoIncrement: true,
+        allowNull: false;
+    },
+    nome:{
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+    email :{
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+    senha : {
+        type: Datatypes.STRING,
+        allowNull: false,
+    }
+});
+
+
+
+
+Usuario.sync({alter: true, force: false})
+    .then(() => {
+        console.log("Tabela de Usuarios foi (re)criada");
+    })
+    .catch((err) => console.log(err));
+module.exports = Usuario;
