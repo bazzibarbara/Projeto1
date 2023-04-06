1f92f65af50e939353da886348bcd01b1c04ab23
unknown <barbararamosberno@outlook.com>
2023-04-05 21:53:07 -0300

    movendo tudo para uma pasta de musica

--- src/domains/Musica/models/Musica.js
+++ src/domains/Musica/models/Musica.js
@@ -33,3 +33,37 @@ Usuario.sync({alter: true, force: false})
 .catch((err) => console.log(err));
 
 module.exports = Musica;
+/*const listaDeMusicas = [
+    {
+        nome: 'Dark Necessities',
+        artista: 'Red Hot Chili Peppers',
+        genero: 'Rock',
+        quantidadeDownloads: 15000,
+    },
+    {
+        nome: 'Boate Azul',
+        artista: 'Bruno & Marrone',
+        genero: 'Sertanejo',
+        quantidadeDownloads: 1000,
+    },
+    {
+        nome: 'Power',
+        artista: 'Kanye West',
+        genero: 'Hip Hop',
+        quantidadeDownloads: 12000,
+    },
+    {
+        nome: 'Money',
+        artista: 'Pink Floyd',
+        genero: 'Rock',
+        quantidadeDownloads: 140500,
+    },
+    {
+        nome: 'Enemy',
+        artista: 'Imagine Dragons x J.I.D',
+        genero: 'Electropop',
+        quantidadeDownloads: 18000,
+    }
+];
+
+module.exports = listaDeMusicas;*/
