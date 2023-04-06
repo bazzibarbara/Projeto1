bf444051e3e522a3a9f5d10f297d9868cd4e5d4e
Vitor Costa <73201828+vitorcosuta@users.noreply.github.com>
2023-04-05 23:11:54 -0300

    ref: adicionando chaves em blocos if

--- src/domains/Musicas/services/MusicaService.js
+++ src/domains/Musicas/services/MusicaService.js
@@ -7,8 +7,9 @@ class MusicaService{
         
         const musica = Musica.find(musica => musica.nome == nome);
 
-        if(!musica)
+        if(!musica){
             throw new Error('Musica nao encontrada');
+        }
 
         const musica_index = Musica.indexOf(musica);  //Obtendo o index da musica a ser deletada
         Musica.splice(musica_index, 1); // Deleta 1 elemento a partir do index obtido, ou seja, o proprio elemento
@@ -18,19 +19,36 @@ class MusicaService{
     async getMusicaByNome(nome){
         const musica = Musica.find(musica => musica.nome === nome);
 
-        if(!musica)
+        if(!musica){
             throw new Error('Musica nao encontrada.');
+        }
+            
         return musica;
     }
 
     /**@brief Adiciona uma musica ao banco.*/
     async adicionarMusica(req_body){
         
-        if(!req_body)
+        if(!req_body){
             throw new Error('Musica sem dados nao pode ser adicionada.');
+        }
             
         Musica.push(req_body);
     }
+
+    /**@brief Filtra uma musica pelo nome e altera o seu numero de downloads.*/
+    async editarNumDownloads(nome, quant_downloads){
+        const musica = Musica.find(musica => musica.nome === nome);
+
+        if (!musica) {
+            throw new Error('Musica nao encontrada.')
+        }
+
+        musica.quantidadeDownloads = quant_downloads;
+
+    }
+
+
 }
 
 module.exports = new MusicaService;
