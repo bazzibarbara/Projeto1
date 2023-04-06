1dd8e8bc2c191a4e1ce1544dbe972d8d36dbab0f
unknown <barbararamosberno@outlook.com>
2023-04-06 09:16:42 -0300

    feat construção cruds de usuario

--- src/domains/usuarios/controllers/UsuarioController.js
+++ src/domains/usuarios/controllers/UsuarioController.js
@@ -5,66 +5,60 @@ const Usuario = require('../models/Usuario');
 const Usuario = require('../models/Usuario');
 const UsuarioService = require('../service/UsuarioService');
 
-router.post('/', async(req,res) =>{
-    const body = req.body;
+
+//cria usuario(C do CRUD)
+router.post('/create', async(req,res) => {
     try {
+        const resultado = await database.sync();
+        console.log(resultado);
+        const criarUsuario = await Usuario.create({
+            nome: 
+            email: 
+            senha:
+            cargo:
+        })
+        console.log(criarUsuario);
         await Usuario.create(body);
-        return res.status(201).json('Usuario criado com sucesso');
-    } catch (error) {
-        return res.status(400);
-        
-    };
-})
-
-//cria usuario
-try {
-    const resultado = await database.sync();
-    console.log(resultado);
-
-    const criarUsuario = await Usuario.create({
-        nome: 
-        email: 
-        senha:
-        cargo:
-    })
-    console.log(criarUsuario);
+    return res.status(201).json('Usuario criado com sucesso');
 } catch (error) {
-    console.log(error).status(400);
-};
-//modulo read
+    return res.status(400);
+}
+)}
+
+
+//modulo read todos os usuarios (R DO CRUD)
+router.get('/todos', (req,res) => {
 const lerUsuario = await Usuario.findAll();
 console.log(lerUsuario);
 res.status(200);
 if(!Usuario){
     res.status(400).json({message: 'Usuario não existe'});
 }
+})
+//modulo read pelo id do usuario 
+router.get('/:id', (req,res) => {
+const lerUsuarioId = await Usuario.findByPk();
+console.log(lerUsuarioId);
+res.status(200);
+if(!Usuario){
+    res.status(400).json({message: 'Id não encontrado'});
+}
+})
 
-
-//atualiza um usuario
-router.put('edit/:nome/', (req,res) =>{
+//atualiza um usuario (U do crud)
+router.put('edit/:nomenovo/', (req,res) =>{
     const {nome} = req.params;
 if(!Usuario){
     res.status(400).json({message: 'Usuario não existe'});}
 
-    const Usuario = Usuario.find(Usuario => Usuario.nome === nome);
-    Usuario.nome = nome;
-    res.status(200).json(usuario);
-)};
+    const Usuario = Usuario.find(Usuario => Usuario.nome === nomenovo);
+    Usuario.nome = nomenovo;
+   await Usuario.save();
+    res.status(200).json('nome alterado com sucesso');
+})
 
-// edita a quantidade de downloads de uma musica pelo nome
-router.put('/edit/:nome/:quantidadeDownloads', (req, res) => {
-    const { nome, quantidadeDownloads } = req.params;
 
-    if(!musica) return res.status(404).json();
-
-    musica.quantidadeDownloads = quantidadeDownloads;
-
-});
-
-const resultadoSave = await Usuario.save();
-console.log(resultadoSave);
-
-//deletar usuario pelo nome
+//deleta um usuario pelo nome (D do crud)
 router.delete('/delete/:nome', async(req,res) =>{
     const { nome } = req.params;
 try{
