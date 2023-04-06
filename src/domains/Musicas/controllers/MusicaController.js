6b839acc8950016cad34f0a8ad1c8fb44563e74a
Vitor Costa <73201828+vitorcosuta@users.noreply.github.com>
2023-04-06 00:02:24 -0300

    feat: CRUD Musica

--- src/domains/Musicas/controllers/MusicaController.js
+++ src/domains/Musicas/controllers/MusicaController.js
@@ -1,9 +1,16 @@
-const router = require('express').Router();
+const express = require ('express');
+const router = express.Router();
 const Musica = require('../models/Musica');
 const MusicaService = require('../services/MusicaService');
 
-router.get('/all', (req,res) => {
-    res.status(200).send(Musica);
+router.get('/all', async (res) => {
+    try{
+        const musicas = await MusicaService.obterMusicas();
+        res.status(200).send(musicas);
+    }catch{
+        res.status(400).json('Nao foi possivel acessar a tabela de musicas.');
+    }
+    
 });
 
 // retorna dados de uma musica pelo nome
@@ -11,10 +18,10 @@ router.get('/all/:nome', async (req,res) =>{  //obrigatoriamente precisa passar 
     const { nome } = req.params;
     
     try{
-        let musica = await MusicaService.getMusicaByNome(nome);
+        const musica = await MusicaService.getMusicaByNome(nome);
         res.status(200).json(musica);
     }catch{
-        res.status(404).json();
+        res.status(400).json(`Nao foi encontrada musica com o nome ${nome}.`);
     }
 });
 
@@ -22,21 +29,21 @@ router.get('/all/:nome', async (req,res) =>{  //obrigatoriamente precisa passar 
 router.post('/add', async (req, res) => {
     try{
         await MusicaService.adicionarMusica(req.body);
-        res.status(200).send('Musica adicionada com sucesso');
+        res.status(201).json('Musica adicionada com sucesso.');
     }catch{
-        res.status(404).send('Musica nao encontrada');
+        res.status(400).send('Nao foi possvel adicionar a musica.');
     }
 });
 
 // edita a quantidade de downloads de uma musica pelo nome
-router.put('/edit/:nome/:quantidadeDownloads', (req, res) => {
+router.put('/edit/:nome/:quantidadeDownloads', async (req, res) => {
     const { nome, quantidadeDownloads } = req.params;
     
     try{
         await MusicaService.editarNumDownloads(nome, quantidadeDownloads);
         res.status(200).send(`Numero de downloads da musica ${nome} editado com sucesso.`);
     }catch{
-        res.status(404).json();
+        res.status(400).json();
     }
 
 });
@@ -44,12 +51,13 @@ router.put('/edit/:nome/:quantidadeDownloads', (req, res) => {
 // deleta uma musica pelo nome
 router.delete('/delete/:nome', async (req, res) => {
     const { nome } = req.params;
+    
     try{
         await MusicaService.deletarMusica(nome);
         res.status(200).send('Musica deletada com sucesso');
     }
     catch{
-        res.status(404).send('Musica nao encontrada');
+        res.status(400).send('Musica nao encontrada');
     }
 });
 
--- src/domains/Musicas/services/MusicaService.js
+++ src/domains/Musicas/services/MusicaService.js
@@ -5,21 +5,24 @@ class MusicaService{
     /**@brief Deleta uma musica.*/
     async deletarMusica(nome){
         
-        const musica = Musica.find(musica => musica.nome == nome);
+        const musica = await Musica.findOne({ where: { nome: `${nome}`} });
 
-        if(!musica){
-            throw new Error('Musica nao encontrada');
+        if (!musica){
+            throw new Error('Musica nao encontrada.');
         }
 
-        const musica_index = Musica.indexOf(musica);  //Obtendo o index da musica a ser deletada
-        Musica.splice(musica_index, 1); // Deleta 1 elemento a partir do index obtido, ou seja, o proprio elemento
+        Musica.destroy({ where: { nome: `${nome}` } })
+    }
+
+    async obterMusicas(){
+        return await Musica.findAll();
     }
 
     /**@brief Busca uma musica no banco de dados pelo nome.*/
-    async getMusicaByNome(nome){
-        const musica = Musica.find(musica => musica.nome === nome);
+    async obterMusicaPorNome(nome){
+        const musica = await Musica.findOne({ where: { nome: `${nome}`} });
 
-        if(!musica){
+        if (!musica){
             throw new Error('Musica nao encontrada.');
         }
             
@@ -29,22 +32,23 @@ class MusicaService{
     /**@brief Adiciona uma musica ao banco.*/
     async adicionarMusica(req_body){
         
-        if(!req_body){
+        if (!req_body){
             throw new Error('Musica sem dados nao pode ser adicionada.');
         }
-            
-        Musica.push(req_body);
+        
+        await Musica.create(req_body);
     }
 
     /**@brief Filtra uma musica pelo nome e altera o seu numero de downloads.*/
     async editarNumDownloads(nome, quant_downloads){
-        const musica = Musica.find(musica => musica.nome === nome);
+        const musica = await Musica.findOne({ where: { nome: `${nome}`} });
 
-        if (!musica) {
+        if (!musica){
             throw new Error('Musica nao encontrada.')
         }
 
         musica.quantidadeDownloads = quant_downloads;
+        await musica.save();
 
     }
 
