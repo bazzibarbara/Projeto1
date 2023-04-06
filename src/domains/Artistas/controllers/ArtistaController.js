67299b6256efc03c1307c5cc3c95aa3b061ba9e8
iagozag <iagozag@gmail.com>
2023-04-06 09:59:01 -0300

    branch main deletada

--- src/domains/Artistas/controllers/ArtistaController.js
+++ src/domains/Artistas/controllers/ArtistaController.js
@@ -1,7 +1,17 @@
 const router =  require('express').Router();
 const ArtistaService = require('../service/ArtistaService');
 
-router.post('/', async(req,res) =>{
+router.post('/artistas', async(req,res) =>{
+    const body = req.body;
+    try {
+        await ArtistaService.adicionarArtista(body);
+        return res.status(201).json('Artistas criado com sucesso');
+    } catch {
+        return res.status(400);
+    }
+});
+
+router.get('/', async(req,res) =>{
     const body = req.body;
     try {
         await ArtistaService.adicionarArtista(body);
--- src/domains/Artistas/services/ArtistaService.js
+++ src/domains/Artistas/services/ArtistaService.js
@@ -3,7 +3,13 @@ const Artistas = require('../models/Artista');
 class ArtistasService{
     async adicionarArtista(body){
         await Artistas.create(body);
-    } 
+    }
+
+    async obterArtistas(){
+        return await Artistas.findAll();
+    }
+
+    
 }
 
 module.exports = new ArtistasService();
