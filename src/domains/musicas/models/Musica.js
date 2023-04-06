eb54b15a7fe5a0a6befff5e764058bc3e0ceabe0
iagozag <iagozag@gmail.com>
2023-04-05 22:44:44 -0300

    models Musica corrigido

--- src/domains/musicas/models/Musica.js
+++ src/domains/musicas/models/Musica.js
@@ -1,35 +1,36 @@
-const sequelize = require('../../../../database/index');
-const {Datatypes} = require('sequelize');
+const database = require('../../../../database/index');
+const Sequelize = require('sequelize');
 
-const Musica = sequelize.define('Musica', {
+const Musica = database.define('Musica', {
     id:{
-        type:Datatypes.INTEGER,
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
         allowNull: false,
-
     },
     foto:{
-        type: Datatypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
 
     },
     nacionalidade :{
-        type: Datatypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
     },
     titulo: {
-        type: Datatypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
     },
     categoria: {
-        type: Datatypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
     }
 });
 
 Musica.sync({alter: true, force: false})
-.then(() => {
-    console.log("Tabela de Musica foi (re)criada");
-})
-.catch((err) => console.log(err));
+    .then(() => {
+        console.log('Tabela de Musica foi (re)criada');
+    })
+    .catch((err) => console.log(err));
 
 module.exports = Musica;
