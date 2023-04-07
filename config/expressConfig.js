68d2a392f03be19f4b1579af54e51b52fcda695b
unknown <barbararamosberno@outlook.com>
2023-04-07 12:57:12 -0300

    fix erros pontuais

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -11,7 +11,7 @@ const musicaRouter=  require('../src/domains/Musicas/controllers/MusicaControlle
 const artistaRouter=  require('../src/domains/Artistas/controllers/ArtistaController');
 const usuarioRouter=  require('../src/domains/Usuarios/controllers/UsuarioController');
 app.use('/api/musica', musicaRouter);
-app.use('/api/musica', artistaRouter);
-app.use('/api/musica', usuarioRouter);
+app.use('/api/artista', artistaRouter);
+app.use('/api/usuario', usuarioRouter);
 
 module.exports = app;
