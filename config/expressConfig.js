b2c4d3b07b1e0145745716b9a5dc5fe97456ba03
unknown <barbararamosberno@outlook.com>
2023-04-21 06:47:03 -0300

    middleware cors

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -1,7 +1,14 @@
 require('express-async-errors');
 const express = require ('express');
-
 const app = express();
+
+const cors = require('cors');
+app.use(cors(
+{
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+},
+));
 
 app.use(express.json());
 app.use(express.urlencoded({
