ec6cfb0215bcc0065180ed89958994c73093a190
unknown <barbararamosberno@outlook.com>
2023-04-21 07:07:25 -0300

    feat função encryptPassword

--- src/domains/Usuarios/service/UsuarioService.js
+++ src/domains/Usuarios/service/UsuarioService.js
@@ -1,7 +1,44 @@
+const { QueryError } = require('sequelize');
 const Usuario = require('../models/Usuario');
 
+async create(body) {
+    if (body.role == userRoles.admin){
+        throw new PermissionError('Não é possivel criar um usuário com cargo de administrador');
+    }
+    const user = await User.findOne{(where:{ email: body.email})};
+    if(user){
+        throw new QueryError('E-mail já cadastrado');
+
+    } else {
+        const user = {
+            name: body.name, 
+            email: body.email, 
+            password: body.password,
+            role: body.role, 
+        };
+        user.password = await this.encryptPassword(body.password);
+       
+        await User.create(user);
+    }
+}
+
+
+
+
+
+
+
+
 class UsuarioService{
-    
+     
+    async encryptPassword(password) {
+        const saltRounds = 10;
+        const encryptPassword = await bcrypt.hash(password, saltRounds);
+        return encryptPassword;
+    }
+
+
+
     /**@brief Busca no banco todos os usuarios cadastrados.*/
     async obterUsuarios(){
         return await Usuario.findAll();
