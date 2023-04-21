c6ea5536c69d350ae855f5c6d23b91b39dbd9756
unknown <barbararamosberno@outlook.com>
2023-04-21 09:46:54 -0300

    feat implementando jsonwebtoken  e instalação

--- src/middlewares/auth-middlewares.js
+++ src/middlewares/auth-middlewares.js
@@ -13,4 +13,42 @@ function generateJWT(user, res){
             httpOnly: true,
             secure: process.env.NODE_EMV !== 'development',
         });
+        function cookieExtractor(req) {
+            let token = null;
+            if(req && req.cookies){
+                token = req.cookies['jwt'];
+            }
+            return token;
+        }
+
+        function verifyJWT(req,res,next){
+            try{
+                const token = cookieExtractor(req);
+                if(token){
+                    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+                    req.user = decoded.user;
+                }
+                if(!req.user){
+                    throw new PermissionError(
+                        'Você precisa estar logado para realizar essa ação!');
+                }
+                next();
+            } catch(error) {
+                next(error);
+            }
+        }
+        async function loginMiddleware(req, res, next ){
+            try {
+                const user =await User.findOne({ where: {email: req.body.email}});
+                if (!user) {
+                    throw new PermissionError('E-mail e/ou senha incorretos');
+                } else {
+                    const matchingPassword = await bcrypt.compare(req.body.password, user.password);
+                    if(!matchingPassword) {
+                        throw new PermissionError('E-mail e/ou senha incorretos');
+                    }
+                }
+            }
+            generateJWT(user, res);
+        }
 }
